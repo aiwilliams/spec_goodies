16e81590b929366836a53f657c7ed06ce2f39143
John W. Long <me@johnwlong.com>
2007-11-03 15:24:01 +0000

    spec_goodies: removed spurious reference to ExampleModule

--- lib/spec/goodies/dsl/extensions/example.rb
+++ lib/spec/goodies/dsl/extensions/example.rb
@@ -1,9 +1,9 @@
 Spec::DSL::Example.module_eval do
   include Spec::Goodies::Matchers
 end
-Spec::DSL::ExampleModule.module_eval do
-  include Spec::Goodies::Matchers
-end
+#Spec::DSL::ExampleModule.module_eval do
+#  include Spec::Goodies::Matchers
+#end
 Test::Unit::TestCase.module_eval do
   include Spec::Goodies::Matchers
 end
