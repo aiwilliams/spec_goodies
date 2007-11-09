108cb27d8b7bb0e56a6edd3051225e956fa2be3c
Christopher <redinger@gmail.com>
2007-11-09 16:32:02 +0000

    spec_goodies: made to work with rspec trunk rev 2835

--- lib/spec/goodies/dsl/extensions/example.rb
+++ lib/spec/goodies/dsl/extensions/example.rb
@@ -1,9 +1,3 @@
-Spec::DSL::Example.module_eval do
-  include Spec::Goodies::Matchers
-end
-#Spec::DSL::ExampleModule.module_eval do
-#  include Spec::Goodies::Matchers
-#end
 Test::Unit::TestCase.module_eval do
   include Spec::Goodies::Matchers
 end
